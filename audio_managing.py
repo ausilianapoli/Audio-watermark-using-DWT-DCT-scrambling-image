7ee980cb49056ed18cf3ec3c7ce91ba1ab9245e5
ausilianapoli <ausilianapoli@gmail.com>
2019-12-05 22:15:59 +0100

    add procedure savefile

--- audio_managing.py
+++ audio_managing.py
@@ -4,6 +4,10 @@ import subprocess as sp
 import os
 import platform
 
+AUDIO_PATH = 0
+SAMPLERATE = 1
+AUDIO_DATA = 2
+
 #Read the file audio.wav from path
 def readWavFile(path):
     samplerate, data = wavfile.read(path)
@@ -13,15 +17,19 @@ def readWavFile(path):
 #Print some information about file audio    
 def printMetadata(entry):
     print("Path: {}"\
-          .format(entry[0]))
+          .format(entry[AUDIO_PATH]))
     print("\tsamplerate: {}"\
-          .format(entry[1]))
+          .format(entry[SAMPLERATE]))
     print("\t#samples: {}"\
-          .format(entry[2].shape))
-    ttl = list(entry[2].shape) #to extract tuple's values as int first it converts into list
-    shape = ttl.pop() #and then it is popped the single element of list
-    print("\tduration: {} seconds"\
-          .format(str(round(shape/entry[1]))))
+          .format(entry[AUDIO_DATA].shape))
+
+#Save processed file audio with wav format
+def saveFile(path, samplerate, signal):
+    fileName = os.path.basename(path)
+    dirName = os.path.dirname(path)
+    fileName = "watermarked_" + fileName
+    path = os.path.join(dirName, fileName)
+    wavfile.write(path, samplerate, signal)
 
 '''
 TESTING
@@ -29,5 +37,6 @@ TESTING
 
 tupleAudio = readWavFile("piano.wav")
 printMetadata(tupleAudio)
+saveFile(tupleAudio[AUDIO_PATH], tupleAudio[SAMPLERATE], tupleAudio[AUDIO_DATA])
 
 
