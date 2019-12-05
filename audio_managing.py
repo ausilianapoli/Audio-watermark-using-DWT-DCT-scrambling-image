27ffdce8e3180ffe2a4c6cf3b5833fef601bf00d
ausilianapoli <ausilianapoli@gmail.com>
2019-12-05 22:05:29 +0100

    add procedure for print audio metadata

--- audio_managing.py
+++ audio_managing.py
@@ -5,15 +5,29 @@ import os
 import platform
 
 #Read the file audio.wav from path
-def readWavFile(self, path):
+def readWavFile(path):
     samplerate, data = wavfile.read(path)
     tupleWav = (path, samplerate, data)
     return tupleWav
 
+#Print some information about file audio    
+def printMetadata(entry):
+    print("Path: {}"\
+          .format(entry[0]))
+    print("\tsamplerate: {}"\
+          .format(entry[1]))
+    print("\t#samples: {}"\
+          .format(entry[2].shape))
+    ttl = list(entry[2].shape) #to extract tuple's values as int first it converts into list
+    shape = ttl.pop() #and then it is popped the single element of list
+    print("\tduration: {} seconds"\
+          .format(str(round(shape/entry[1]))))
 
 '''
 TESTING
 '''
 
 tupleAudio = readWavFile("piano.wav")
+printMetadata(tupleAudio)
+
 
