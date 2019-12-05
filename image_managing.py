9b44b2f6d61d2c618022c346a64780de63cf08ce
Mattia <mattialitrico@gmail.com>
2019-12-05 16:59:43 +0100

    Adding lower triangular mapping

--- image_managing.py
+++ image_managing.py
@@ -2,11 +2,12 @@ from PIL import Image
 import cv2
 from matplotlib import pyplot as plt
 import numpy as np
+import math
 
 #Return numpy array from a Image file
 def loadImage(path):
     if path is "":
-        print("Path must not be None!")
+        print("LOAD IMAGE: Path must not be None!")
         return 
 
     img = Image.open(path)
@@ -14,18 +15,19 @@ def loadImage(path):
     return imgToArray
 
 #Show image from array or path
-def showImage(img=None, path=None):
-    if path is not None:
-        Image.open(path).show()
-    elif img is not None:
+def showImage(img):
+    if type(img) is np.ndarray:
         Image.fromarray(img).show()
     else:
-        print("showImage needs an array or a path!")
+        if img is "":
+            print("SHOW IMAGE: Path must not be None!")
+            return 
+        Image.open(img).show()
 
 #Save image from array or Image file
 def saveImage(img, path):
     if path is "":
-        print("Path must not be None!")
+        print("SHOW IMAGE: Path must not be None!")
         return 
     
     if type(img) is np.ndarray:
@@ -37,19 +39,26 @@ def saveImage(img, path):
 def getImageShape(img):
     return img.shape[1], img.shape[0]
 
+def imodule(a, m):
+    a = a % m
+    for x in range(m):
+        if (a * x) % m == 1:
+            return x   
+    return 1
+
 #Arnold transform
 def arnoldTransform(img, iteration):
-    width, heigth = getImageShape(img)
+    side, _ = getImageShape(img)
     toTransform = img.copy()
     transformed = img.copy()
     
     for iter in range(iteration):
         
-        for i in range(width):
-            for j in range(heigth):
-                newX = (i + j) % width
-                newY = (i + 2*j) % heigth
-                transformed[(newX,newY)] = toTransform[(i,j)]
+        for i in range(side):
+            for j in range(side):
+                newX = (i + j) % side
+                newY = (i + 2*j) % side
+                transformed[(newY,newX)] = toTransform[(j,i)]
         toTransform = transformed.copy()
 
     return transformed
@@ -57,25 +66,73 @@ def arnoldTransform(img, iteration):
 
 #Inverse Arnold transform
 def iarnoldTransform(img, iteration):
+    side, _ = getImageShape(img)
+    transformed = img.copy()
+    toTransform = img.copy()
+    
+    for iter in range(iteration):
+        
+        for i in range(side):
+            for j in range(side):
+                newX = (2*i - j) % side
+                newY = (-i + j) % side
+                transformed[(newY,newX)] = toTransform[(j,i)]
+        toTransform = transformed.copy()
+    return transformed
+
+#2D lower triangular mapping
+def triangularMappingTransform(img, iteration, a, c, d):
     width, heigth = getImageShape(img)
-    itransformed = img.copy()
-    itoTransform = img.copy()
+    transformed = img.copy()
+    toTransform = img.copy()
     
     for iter in range(iteration):
         
         for i in range(width):
             for j in range(heigth):
-                newX = (2*i - j) % width
-                newY = (-i + j) % heigth
-                itransformed[(newX,newY)] = itoTransform[(i,j)]
-        itoTransform = itransformed.copy()
-    return itransformed
+                newX = (a*i) % width
+                newY = (c*i + d*j) % heigth
+                transformed[(newY,newX)] = toTransform[(j,i)]
+
+        toTransform = transformed.copy()
+    
+    return transformed
+    
+#2D inverse lower triangular mapping
+def itriangularMappingTransform(img, iteration, a, c, d):
+    width, heigth = getImageShape(img)
+    transformed = img.copy()
+    toTransform = img.copy()
+    ia = imodule(a, width)
+    id = imodule(d, heigth)
+    for iter in range(iteration):
+        
+        for i in range(width):
+            for j in range(heigth):
+                newX = (ia*i) % width
+                newY = (id*(j + (math.ceil(c*width/heigth)*heigth) - (c*newX))) % heigth
+                transformed[(newY,newX)] = toTransform[(j,i)]
+
+        toTransform = transformed.copy()
+    return transformed
+
+#to write find numeri primi fra loro
 
 #Esempio
-
+'''
+TESTING
+'''
 img = loadImage("right.png")
+imgr = loadImage("07.jpg")
+
 t = arnoldTransform(img,1)
 showImage(t)
 
 it = iarnoldTransform(t,1)
 showImage(it)
+
+m = triangularMappingTransform(imgr,1,5,1,7)
+showImage(m)
+
+im = itriangularMappingTransform(m,1,5,1,7)
+showImage(im)
